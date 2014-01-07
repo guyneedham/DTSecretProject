2f18f5a6bb7db0f66442541fc1ef3dce340557c1
ben.sutton <ben.sutton4@googlemail.com>
2014-01-07 22:33:59 +0000

    First trial commit..Yes i realise it is using java 1.5!!

---
+++ BigIssue/src/main/java/com/BGB/BigIssue/App.java
@@ -0,0 +1,13 @@
+package com.BGB.BigIssue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
