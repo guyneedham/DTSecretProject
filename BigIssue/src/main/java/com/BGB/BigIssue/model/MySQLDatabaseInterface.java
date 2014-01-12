9844f5db92ecceb4c23064d7a94625d3b8f25f76
Brett <brett.christoforo@gmail.com>
2014-01-12 17:52:57 +0000

    Added a few methods that might be used

--- BigIssue/src/main/java/com/BGB/BigIssue/model/MySQLDatabaseInterface.java
+++ BigIssue/src/main/java/com/BGB/BigIssue/model/MySQLDatabaseInterface.java
@@ -1,8 +1,20 @@
 package com.BGB.BigIssue.model;
 
-public interface MySQLDatabaseInterface {
-	
+import java.sql.Date;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 
-	
+public interface MySQLDatabaseInterface {
+
+	public void addVendor(String firstname, String lastname);
+	public void removeVendor(String firstname, String lastname);
+	public void vendorAddsTransaction(int badgeID, int hubLocation, int totalBought, DecimalFormat totalCash, Date DOT);
+	public int getVendorIDFromName(String firstname, String lastname);
+	public void addPitchToVendor(int badge, int pitch);
+	public ArrayList listOfUnregisteredPitches(); //arraylist of pitches
+	public void vendorAddsToSavings(String firstname, String lastname, DecimalFormat moneyIn);
+	public void vendorWithdrawsFromSavings(String firstname, String lastname, DecimalFormat moneyOut);
+	public ArrayList publishBadgeHistory(int badgeID); //arraylist of pitches
+	public ArrayList publishVendorHistory(int vendorID); //arraylist of badges
 
 }
